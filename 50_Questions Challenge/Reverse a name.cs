c27c518c79569aa807dcf75f9c7d3d6a096df416
Rose Davidson <rosedavidson0502@gmail.com>
2021-09-15 12:15:20 +0530

    Alternative to convert char array to string

--- 50_Questions Challenge/Reverse a name.cs
+++ 50_Questions Challenge/Reverse a name.cs
@@ -9,6 +9,8 @@ public class Prog
         char[] s2 = s.ToCharArray();
         Array.Reverse(s2); //Because here we intend to reverse the letters of the word
         string s3 = new string(s2);
+        //method 2 : Char[] -> String 
+        //string s = String.Concat(c);
         Console.WriteLine(s3);
     }
 
