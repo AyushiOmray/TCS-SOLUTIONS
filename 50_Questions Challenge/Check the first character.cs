7d75cb74c044f3a20e755c486658bfc4faf012a2
Rose Davidson <rosedavidson0502@gmail.com>
2021-09-19 23:39:10 +0530

    Check the first character problem

---
+++ 50_Questions Challenge/Check the first character.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace myapp
+{
+    class Program
+    {
+      public static void Main(){
+         var s = Console.ReadLine();
+         char c = s[0];
+         int i;
+         if(c>='a' && c<='z'){
+             Console.WriteLine("Lower Alphabet");
+         }
+         else if(c>='A' && c<='Z'){
+             Console.WriteLine("Upper Alphabet");
+         }
+         else if(Int32.TryParse(c.ToString(),out i)){
+             Console.WriteLine("Number");
+         }
+         else{
+         Console.WriteLine("Character");
+         }
+      }
+    }
+}
