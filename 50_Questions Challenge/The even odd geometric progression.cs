89d4565f55b8194d09831b3d8a59d0ef17c60fed
Rose Davidson <rosedavidson0502@gmail.com>
2021-09-26 19:25:07 +0530

    the even odd GP Problem

---
+++ 50_Questions Challenge/The even odd geometric progression.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+class MyClass{
+    public static void Main(){
+        //1 2 4 8......{1 3 5 7}
+        //1 3 9 27.....{2 4 6 8}
+        //1 1 2 3 4 9 8.......
+        
+        int n = Int32.Parse(Console.ReadLine());
+       
+            
+            if(n%2==0){
+                int p = (n/2)-1;
+                Console.WriteLine(Math.Pow(3,p));
+            }
+            else{
+                int p = (n/2);
+                Console.WriteLine(Math.Pow(2,p));
+            }
+        
+        
+    }
+}
