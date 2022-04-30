82797624390231849f8391e16d673cf079019c90
AYUSHIOMRAY <ayushiomray@gmail.com>
2022-04-30 17:44:57 +0530

    created basic program and establishing basic connections

---
+++ C# Revisit/HelloWorld2.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld{
+ class Program{
+     public static void Main(String[] args){
+         Console.WriteLine("Hello World, Good to be Back!");
+     }
+ }
+}
